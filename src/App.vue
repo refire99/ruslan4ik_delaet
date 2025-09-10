a115fa8b8317a00e6bda4c3716f8aaf6a469ac1b
Your Name <you@example.com>
2025-09-10 11:29:29 +0300

    aga

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header.vue';
+import catalog_button from './components/catalog_button.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Даша жопа" />
+
+  <HelloWorld msg="okak" />
+  <Header />
+  <catalog_button/>
 </template>
 
 <style scoped>
---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+</script>
+
+<template>
+    <div class="header">
+       <div class="logo">
+
+       </div>
+       <div class="catalog_button">
+
+       </div>
+       <div class="search_input">
+
+       </div>
+
+    </div>
+</template>
+
+<style scoped>
+.header{
+    color: white;
+    width: 100%;
+    height: 72px;
+    background: black;
+    justify-content: space-between;
+    align-items: center;
+    display: flex;
+}
+</style>
